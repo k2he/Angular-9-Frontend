8da849903aa276e9077908bfed42858dad627326
k2he <he_kai517@hotmail.com>
2018-04-21 16:31:32 -0400

    Fixing page layout, css

--- angular5-client/src/app/login/login.component.ts
+++ angular5-client/src/app/login/login.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { trigger, style, animate, transition } from '@angular/animations';
+
 import { AuthenticationService } from "../core/service/authentication.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [
+    trigger('showUp', [
+      transition('void => *', [
+        style({
+          opacity: 0.3
+        }),
+        animate('200ms')
+      ])
+    ])
+  ]
 })
 export class LoginComponent implements OnInit {
 
   username : string;
   password : string;
+  showSpinner: boolean;
+  loginFailErrorMessage: string;
 
   constructor(private router : Router,
               private authService: AuthenticationService) { }
@@ -19,17 +33,21 @@ export class LoginComponent implements OnInit {
   }
 
   private login() : void {
+    this.showSpinner = true;
+    this.loginFailErrorMessage = "";
+    
     this.authService.login(this.username, this.password)
             .subscribe(() => {
+              this.showSpinner = false;
                 this.router.navigate(['/home']);
             }, e => this.handleError(e));
   }
 
-  //Need figure out how to handle error handle show on page
+  //Need figure out how to get correct error so we can highlight corrsponding input box.
   private handleError(error) {
     switch (error.status) {
         case 401:
-            alert("401");
+          this.loginFailErrorMessage = "Login failed. Please make sure username and password is correct.";
     }
 }
 
