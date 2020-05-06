b79e71a9738c960ac61038163a1f271f85959dfc
k2he <he_kai517@hotmail.com>
2020-05-06 14:47:36 -0400

    add cloud environment file back

---
+++ angular9-client/src/environments/environment.cloud.ts
@@ -0,0 +1,15 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  
+  apiPath: "https://www.kaihetech.com/api",
+
+  DEFAULT_AUTH_URL: 'http://www.kaihetech.com:8080/api/login',
+  GOOGLE_AUTH_URL: 'http://www.kaihetech.com:8080/oauth2/authorize/google?redirect_uri=https://www.kaihetech.com/home',
+  FACEBOOK_AUTH_URL: 'http://www.kaihetech.com:8080/oauth2/authorize/facebook?redirect_uri=https://www.kaihetech.com/home',
+  GITHUB_AUTH_URL: 'http://www.kaihetech.com:8080/oauth2/authorize/github?redirect_uri=https://www.kaihetech.com/home',
+};
