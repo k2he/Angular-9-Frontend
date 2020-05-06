6e29823d6c1d6ffc56462b6da949f72b367970c7
k2he <he_kai517@hotmail.com>
2020-05-05 20:22:00 -0400

    update Angular material to version 9

--- angular5-client/src/app/projects/all-projects/all-projects.component.ts
+++ angular5-client/src/app/projects/all-projects/all-projects.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, AfterViewChecked } from '@angular/core';
-import { MatTableDataSource, MatSort } from '@angular/material';
-import { MatDialog, MatDialogRef } from '@angular/material';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
 
 import { ProjectService } from '../../api/project.service';
